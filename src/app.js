75cc24fc1924b17dd375ce18df8aa11d477f44fe
manpreetsinghbhikhe <manpreet.singhbhikhe@honestfoodcompany.de>
2020-10-02 11:22:58 +0530

    Event handling and form

--- src/app.js
+++ src/app.js
@@ -3,55 +3,52 @@ console.log('App.js is running');
 const app = {
     title: 'Indecision App',
     subtitle: 'Put your life in the hands of a computer!',
-    options: ['One', 'Two', 'Three']
+    options: []
 };
 
-const template = (
-    <div>
-        <h1>{app.title}</h1>
-        {app.subtitle && <p>{app.subtitle}</p>}
-        <p>{app.options.length > 0 ? "Here are your options" : "No options"}</p>
-        <ol>
-            <li>Item one</li>
-            <li>Item two</li>
-            <li>Item three</li>
-        </ol>
-    </div>
-);
+const onFormSubmit = (e) => {
+    e.preventDefault();
 
-// ------------------------------------------------------------------------------
-// JSX doesn't have in-built data-binding
-// Code to showcase manual data-binding & re-rendering
+    const option = e.target.elements.option.value;
+    
+    if(option){
+        app.options.push(option);
+    }
+    
+    // required because reactDom only renders parts that have been modified.
+    // try commenting below line and see difference for yourself
+    e.target.elements.option.value = '';
+    
+    renderApp();
 
-let count = 0;
-
-const addOne = () => {
-    count++;
-    renderCounterApp();
-};
-const minusOne = () => {
-    count--;
-    renderCounterApp();
-};
-const reset = () => {
-    count = 0;
-    renderCounterApp();
 };
 
-const renderCounterApp = () => {
-    const templateTwo = (
+const removeAll = () => {
+    app.options = [];
+    renderApp();
+};
+
+const renderApp = () => {
+    const template = (
         <div>
-            <h1>Count: {count}</h1>
-            <button onClick={addOne}>+1</button>
-            <button onClick={minusOne}>-1</button>
-            <button onClick={reset}>Reset</button>
+            <h1>{app.title}</h1>
+            {app.subtitle && <p>{app.subtitle}</p>}
+            <p>{app.options.length > 0 ? "Here are your options" : "No options"}</p>
+            <p>{app.options.length}</p>
+            <button onClick={removeAll}>Remove All</button>
+            <ol>
+                <li>Item one</li>
+                <li>Item two</li>
+                <li>Item three</li>
+            </ol>
+            <form onSubmit={onFormSubmit}>
+                <input type="text" name="option"/>
+                <button>Add option</button>
+            </form>
         </div>
     );
-
-    ReactDOM.render(templateTwo, document.getElementById('app'));
+    
+    ReactDOM.render(template, document.getElementById('app'));
 };
 
-renderCounterApp();
-// ------------------------------------------------------------------------------
-
-// ReactDOM.render(template, document.getElementById('app'));
+renderApp();
