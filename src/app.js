aa42ec8c6ac6160a9a6ef94d1faf8be4db9d5068
manpreetsinghbhikhe <manpreet.singhbhikhe@honestfoodcompany.de>
2020-10-05 20:34:46 +0530

    React components challenge 1

--- src/app.js
+++ src/app.js
@@ -1,58 +1,58 @@
-console.log('App.js is running');
-
-const app = {
-    title: 'Indecision App',
-    subtitle: 'Put your life in the hands of a computer!',
-    options: []
-};
-
-const onFormSubmit = (e) => {
-    e.preventDefault();
-
-    const option = e.target.elements.option.value;
-    
-    if(option){
-        app.options.push(option);
-    }
-    
-    e.target.elements.option.value = '';
-    
-    renderApp();
-
-};
-
-const removeAll = () => {
-    app.options = [];
-    renderApp();
-};
-
-const pickOption = () => {
-    const index = Math.floor(Math.random() * app.options.length);
-    alert(app.options[index]);
-};
-
-// JSX arrays require a key attribute so that react can diff and manage parts for re-rendering
-const renderApp = () => {
-    const template = (
-        <div>
-            <h1>{app.title}</h1>
-            {app.subtitle && <p>{app.subtitle}</p>}
-            <p>{app.options.length > 0 ? "Here are your options" : "No options"}</p>
-            <button disabled={app.options.length === 0} onClick={pickOption}>What should i do ?</button>
-            <button onClick={removeAll}>Remove All</button>
-            <ol>
-                {
-                    app.options.map((option, index) => <li key={index}>{option}</li>)
-                }
-            </ol>
-            <form onSubmit={onFormSubmit}>
-                <input type="text" name="option"/>
-                <button>Add option</button>
-            </form>
-        </div>
+class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Indecision App</h1>
+        <h2>Put your life in the hands of a computer!</h2>
+      </div>
     );
-    
-    ReactDOM.render(template, document.getElementById('app'));
-};
+  }
+}
 
-renderApp();
+class Action extends React.Component {
+  render() {
+    return (
+      <div>
+        <button>What should I do?</button>
+      </div>
+    );
+  }
+}
+
+// React component challenge 1
+// Options -> Options component here
+// Add Option -> Add option component here
+
+class Options extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>This is options component</p>
+      </div>
+    );
+  }
+}
+
+class AddOption extends React.Component {
+  render() {
+    return (
+      <div>
+        <form>
+          <input type="text" name="option" />
+          <button>Add option</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+const jsx = (
+    <div>
+        <Header/>
+        <Action/>
+        <Options/>
+        <AddOption/>
+    </div>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));
