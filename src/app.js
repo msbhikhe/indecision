a94f13f64a66bddacc51058f1c25cb48e31329c6
manpreetsinghbhikhe <manpreet.singhbhikhe@honestfoodcompany.de>
2021-04-02 22:58:32 +0530

    Dumping regular CSS using style-loader and css-loader

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import './styles/styles.css';
 
 import Action from "./components/Action";
 import AddOption from "./components/AddOption";
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,27 @@
-const path = require('path')
+const path = require("path");
 
 module.exports = {
-    mode: 'development',
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_nodules/
-        }]
-    },
-    devtool: 'eval-cheap-module-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public')
-    }
+  mode: "development",
+  entry: "./src/app.js",
+  output: {
+    path: path.join(__dirname, "public"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        loader: "babel-loader",
+        test: /\.js$/,
+        exclude: /node_nodules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+    ],
+  },
+  devtool: "eval-cheap-module-source-map",
+  devServer: {
+    contentBase: path.join(__dirname, "public"),
+  },
 };
